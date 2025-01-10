e94dae84235ddbdf20a930f1190b792078a5c265
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2025-01-10 08:54:58 +0600

    return

--- src/assets/MyComponent.jsx
+++ src/assets/MyComponent.jsx
@@ -5,9 +5,25 @@ function MyComponent(){
     const [car, setCar] = useState( {year: 2024, 
                                     make: "Ford", 
                                     model: "Mustang"});
+    
+    function handleYearChange(event) {
+
+    } 
+    
+    function handleMakeChange(event) {
+      
+    }    
+
+    function handleModelChange(event) {
+      
+    }    
 
     return( <div>
               <p>Your favourite car is :{car.year} {car.make} {car.model}</p>
+
+              <input type="number" value={car.year}/> <br/>
+              <input type="text" value={car.make}/> <br/>
+              <input type="text" value={car.model}/> <br/>
       
             </div>)
 }
